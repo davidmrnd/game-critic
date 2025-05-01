b946da585c293980871fcc6d5e59f9182c77c85c
Alejandro-Guerra-Jimenez <alejandroguerraj013@gmail.com>
2025-05-01 12:23:42 +0100

    Incorporación de las listas de seguidores y seguidos en las socialstats

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,7 +1,7 @@
-import {DataService} from '../../services/data.service';
-import {ActivatedRoute, RouterModule} from '@angular/router';
-import {CommonModule} from '@angular/common';
-import {Component, Input, OnInit} from '@angular/core';
+import { DataService } from '../../services/data.service';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
@@ -10,8 +10,7 @@ import {Component, Input, OnInit} from '@angular/core';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent  implements OnInit {
-
+export class ProfileComponent implements OnInit, OnChanges {
   @Input() type: string = '';
   data: any = null;
   id!: string;
@@ -19,16 +18,33 @@ export class ProfileComponent  implements OnInit {
   constructor(private dataService: DataService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    // Suscribirse a los parámetros de la ruta
     this.route.queryParams.subscribe(params => {
       this.id = params['id'];
+      this.tryLoadData(); // Intentar cargar los datos si todo está listo
     });
-    console.log(this.id);
+  }
 
-    if(this.type === 'videogame'){
+  ngOnChanges(changes: SimpleChanges): void {
+    // Detectar cambios en las propiedades de entrada
+    if (changes['type']) {
+      this.tryLoadData(); // Intentar cargar los datos si todo está listo
+    }
+  }
+
+  private tryLoadData(): void {
+    // Asegurarse de que tanto 'id' como 'type' estén definidos antes de cargar los datos
+    if (this.id && this.type) {
+      this.loadData();
+    }
+  }
+
+  private loadData(): void {
+    if (this.type === 'videogame') {
       this.dataService.getVideogameById(this.id).subscribe(response => {
         this.data = response;
       });
-    }else{
+    } else {
       this.dataService.getUsersById(this.id).subscribe(response => {
         this.data = response;
       });
